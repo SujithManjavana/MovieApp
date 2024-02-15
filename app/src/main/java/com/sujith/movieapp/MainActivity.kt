72cb123696cb6d6b1fbb9158f5f65c4e0b6b917d
sujithmanjavana <sujithmanjavana@gmail.com>
2024-02-15 17:54:41 +0530

    add movies lazycolumn

--- app/src/main/java/com/sujith/movieapp/MainActivity.kt
+++ app/src/main/java/com/sujith/movieapp/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.sujith.movieapp
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -20,7 +37,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sujith.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,8 +55,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainContent() {
-    Text(text = "Hello")
+fun MainContent(movies: List<String> = listOf("Avatar", "Titanic", "Terminator", "Foo", "Bar")) {
+    Column {
+        LazyColumn {
+            items(items = movies) {
+                MovieItem(movie = it){
+                    Log.e("TAG", "MainContent: $it" )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MovieItem(movie: String, onItemClick: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            // .height(130.dp)
+            .fillMaxWidth()
+            .clickable {
+                onItemClick(movie)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(12.dp), shape = RectangleShape
+            ) {
+                Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Movie image")
+            }
+            Text(text = movie)
+        }
+    }
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
